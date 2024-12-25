71fa1caa33cde317b8371515d0bfe685fc306dc0
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2024-12-25 15:22:06 +0430

    scanning braces

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,10 +5,24 @@ import (
 	"os"
 )
 
+type Token struct {
+	Key   string
+	Value rune
+}
+
 const (
 	LEFT_PAREN  = "LEFT_PAREN"
 	RIGHT_PAREN = "RIGHT_PAREN"
+	LEFT_BRACE  = "LEFT_BRACE"
+	RIGHT_BRACE = "RIGHT_BRACE"
 )
+
+var tokens = map[string]rune{
+	"LEFT_PAREN":  '(',
+	"RIGHT_PAREN": ')',
+	"LEFT_BRACE":  '{',
+	"RIGHT_BRACE": '}',
+}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -17,25 +31,29 @@ func main() {
 	}
 
 	command := os.Args[1]
-
 	if command != "tokenize" {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
 
 	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
+	rawFileContents, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
+	fileContents := []rune(string(rawFileContents))
 	for _, item := range fileContents {
 		switch item {
-		case '(':
-			fmt.Println(LEFT_PAREN, string(item), "null")
-		case ')':
-			fmt.Println(RIGHT_PAREN, string(item), "null")
+		case tokens[LEFT_PAREN]:
+			fmt.Printf("%s %c %s\n", LEFT_PAREN, item, "null")
+		case tokens[RIGHT_PAREN]:
+			fmt.Printf("%s %c %s\n", RIGHT_PAREN, item, "null")
+		case tokens[LEFT_BRACE]:
+			fmt.Printf("%s %c %s\n", LEFT_BRACE, item, "null")
+		case tokens[RIGHT_BRACE]:
+			fmt.Printf("%s %c %s\n", RIGHT_BRACE, item, "null")
 		}
 	}
 	fmt.Println("EOF  null")

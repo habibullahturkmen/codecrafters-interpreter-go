42357ed836e3c7068f6bf6bbeac8fccacc8e1d34
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2024-12-25 15:55:07 +0430

    scanning other single-character tokens

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,16 +5,18 @@ import (
 	"os"
 )
 
-type Token struct {
-	Key   string
-	Value rune
-}
-
 const (
 	LEFT_PAREN  = "LEFT_PAREN"
 	RIGHT_PAREN = "RIGHT_PAREN"
 	LEFT_BRACE  = "LEFT_BRACE"
 	RIGHT_BRACE = "RIGHT_BRACE"
+	COMMA       = "COMMA"
+	DOT         = "DOT"
+	MINUS       = "MINUS"
+	PLUS        = "PLUS"
+	SEMICOLON   = "SEMICOLON"
+	STAR        = "STAR"
+	SLASH       = "SLASH"
 )
 
 var tokens = map[string]rune{
@@ -22,6 +24,13 @@ var tokens = map[string]rune{
 	"RIGHT_PAREN": ')',
 	"LEFT_BRACE":  '{',
 	"RIGHT_BRACE": '}',
+	"COMMA":       ',',
+	"DOT":         '.',
+	"MINUS":       '-',
+	"PLUS":        '+',
+	"SEMICOLON":   ';',
+	"STAR":        '*',
+	"SLASH":       '/',
 }
 
 func main() {
@@ -44,17 +53,35 @@ func main() {
 	}
 
 	fileContents := []rune(string(rawFileContents))
-	for _, item := range fileContents {
-		switch item {
-		case tokens[LEFT_PAREN]:
-			fmt.Printf("%s %c %s\n", LEFT_PAREN, item, "null")
-		case tokens[RIGHT_PAREN]:
-			fmt.Printf("%s %c %s\n", RIGHT_PAREN, item, "null")
-		case tokens[LEFT_BRACE]:
-			fmt.Printf("%s %c %s\n", LEFT_BRACE, item, "null")
-		case tokens[RIGHT_BRACE]:
-			fmt.Printf("%s %c %s\n", RIGHT_BRACE, item, "null")
+	if len(fileContents) > 0 {
+		for _, item := range fileContents {
+			switch item {
+			case tokens[LEFT_PAREN]:
+				fmt.Printf("%s %c %s\n", LEFT_PAREN, item, "null")
+			case tokens[RIGHT_PAREN]:
+				fmt.Printf("%s %c %s\n", RIGHT_PAREN, item, "null")
+			case tokens[LEFT_BRACE]:
+				fmt.Printf("%s %c %s\n", LEFT_BRACE, item, "null")
+			case tokens[RIGHT_BRACE]:
+				fmt.Printf("%s %c %s\n", RIGHT_BRACE, item, "null")
+			case tokens[COMMA]:
+				fmt.Printf("%s %c %s\n", COMMA, item, "null")
+			case tokens[DOT]:
+				fmt.Printf("%s %c %s\n", DOT, item, "null")
+			case tokens[MINUS]:
+				fmt.Printf("%s %c %s\n", MINUS, item, "null")
+			case tokens[PLUS]:
+				fmt.Printf("%s %c %s\n", PLUS, item, "null")
+			case tokens[SEMICOLON]:
+				fmt.Printf("%s %c %s\n", SEMICOLON, item, "null")
+			case tokens[STAR]:
+				fmt.Printf("%s %c %s\n", STAR, item, "null")
+			case tokens[SLASH]:
+				fmt.Printf("%s %c %s\n", SLASH, item, "null")
+			}
 		}
+		fmt.Println("EOF  null")
+	} else {
+		fmt.Println("EOF  null")
 	}
-	fmt.Println("EOF  null")
 }
